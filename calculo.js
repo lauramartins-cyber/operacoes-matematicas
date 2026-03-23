80f79328db9f087e682271d175cff6c3d0988ff5
Laura Martins <martinspiresaline@gmail.com>
2026-03-23 09:37:51 -0300

    Função de subtração

---
+++ calculo.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Digite o primeiro número:");
+let num2 = prompt("Digite o segundo número:");
+
+let resultado = Number(num1) - Number(num2);
+alert("O resultado da subtração é: " + resultado);
